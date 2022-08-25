ecb6c67845110eebabe72558aa4c52fd9381d407
Alex Ju <aju217@gmail.com>
2022-08-25 14:32:25 -0400

    commit tree dacb241271c720d8c3598b0a90e75053ab46e99e

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+*/
+
+/*const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts){
+    for (let i=0; i<gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    return gifts
+}
+wrapGifts(gifts);
+*/
+
+
+const people = [];
+// write people into array before initializing
+const thankYou = [];
+
+function writeCards(people, event){
+    for (let i=0; i< people.length; i++){
+        thankYou[i] =`Thank you, ${people[i]}, for the wonderful ${event} gift!`;
+    }
+    return thankYou
+}
+
+
+
+let number
+
+function countDown (number){
+    while(number >= 0){
+        console.log(number--)
+    }
+}
